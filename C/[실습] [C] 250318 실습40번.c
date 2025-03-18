ad799d5a150ba532a1b75c6f43b6dc39d30045b1
LimKwangyoung98 <young22@daou.co.kr>
2025-03-18 17:02:23 +0900

    250318

---
+++ C/[실습] [C] 250318 실습40번.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <malloc.h>
+
+int main() {
+    int* p1 = malloc(sizeof(int));
+    *p1 = 3;
+
+    int* p2 = realloc(p1, sizeof(int) * 4);
+
+    printf("p1: %p, %d\r\n", p1, *p1);
+    printf("p2: %p, %d\n", p2, *p2);
+
+    free(p2);
+}
