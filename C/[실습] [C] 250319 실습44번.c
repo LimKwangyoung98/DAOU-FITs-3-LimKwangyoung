113c6f3a37256c6a6a54e620e7668e208539c836
LimKwangyoung98 <young22@daou.co.kr>
2025-03-19 17:39:37 +0900

    250319

---
+++ C/[실습] [C] 250319 실습44번.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+
+int main() {
+    FILE* f = fopen("./NewTestFile.txt", "wb");
+
+    if (f) {
+        fclose(f);
+    } else {
+        printf("Error: %d, %s", errno, strerror(errno));
+    }
+
+    return 0;
+}
